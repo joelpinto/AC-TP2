3bbea46f83fb9bd5c0b4cceaa9908ddace00af53
Joel Pinto <joel.pinto.correia@gmail.com>
2016-11-15 17:03:15 +0000

    use of breakpoints now in main

--- main.m
+++ main.m
@@ -1,17 +1,27 @@
 i = 2;
 load 44202;
-inicio = 1;
-fim = 614300;
-T = getTarget(Trg);
-T = transpose(T);
-T = T(1:4, inicio:fim * 0.7);
-SubFeat = FeatVectSel(inicio:fim * 0.7, 1:29);
-SubFeat = transpose(SubFeat);
-Test = FeatVectSel(fim * 0.7:fim, 1:29);
-Test = transpose(Test);
+[T,BreakingPoints] = getTarget(Trg);
+T = T';
+BreakingPoints = BreakingPoints';
+FeatVectSel = FeatVectSel';
+FinalTarget =[];
+FinalIsolated =[];
+for iterator = 1:size(BreakingPoints)
+    before = BreakingPoints(iterator)-1000;
+    after = BreakingPoints(iterator)+ 1000;
+    FinalTarget = [FinalTarget,T(1:4, before:after)];
+    FinalIsolated = [FinalIsolated,FeatVectSel(1:29,before:after)];
+end
+inicio = 0;
+fim = size(FinalIsolated);
+fim = fim(1,2,1);
+TT = FinalTarget(1:4, fim * 0.7:fim);
+T = FinalTarget(1:4, inicio:fim * 0.7);
+SubFeat = FinalIsolated(1:29, inicio:fim * 0.7);
+Test = FinalIsolated(1:29, fim * 0.7:fim);
 
 if i == 1
-    net = newlrn(SubFeat, T, 2);
+    net = layrecnet();
     net.divideParam.trainRatio=1;
     net.divideParam.testRatio=0;
     net.divideParam.valRatio=0;
@@ -20,7 +30,7 @@ if i == 1
     outSim = sim(net,Test);
 end
 if i == 2
-    net = newff(SubFeat, T, 3);
+    net = feedforwardnet(29, 'trainlm');
     net.trainFcn='trainlm';
     net.trainParam.epochs = 100;
     net.trainParam.goal = 0.00001;
@@ -29,4 +39,5 @@ if i == 2
     net.divideParam.valRatio=0;
     net = train(net,SubFeat,T);
     outSim = sim(net,Test);
+    [sensi, speci] = calcPerform(outSim, TT);
 end

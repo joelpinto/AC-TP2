bf544471e597420ec7df9db295de8b212d7306a5
Joel Pinto <joel.pinto.correia@gmail.com>
2016-11-15 17:01:09 +0000

    changed calcPerform

--- calcPerform.m
+++ calcPerform.m
@@ -1,22 +1,23 @@
 function [sensi, speci] = calcPerform(outSim, Target)
     i = 1;
-    Trglength = size(Trg);
+    Trglength = size(Target);
     TP = 0; % true positives
     FN = 0; % false negatives
     TN = 0; % true negatives
     FP = 0; % false positives
     
-    while i < Trglength(1,1,1)
-        if outSim(3,i) == Target(3,i) && Target(3,i) == 1
+    while i < Trglength(1,2,1)
+        if outSim(3,i) > (outSim(1,i) + outSim(2, i) + outSim(4, i)) && Target(3,i) == 1
             TP = TP + 1;
-        elseif outSim(1,i) == Target(1,i) && Target(3,i) == 1
+        elseif outSim(3,i) < (outSim(1,i) + outSim(2, i) + outSim(4, i)) && Target(3,i) ~= 1
             TN = TN + 1;
         end
-        if outSim(3,i) ~= Target(3,i) && Target(3,i) == 1
+        if outSim(3,i) > (outSim(1,i) + outSim(2, i) + outSim(4, i)) && Target(3,i) ~= 1
             FP = FP + 1;
-        elseif outSim(1,i) ~= Target(1,i) && Target(3,i) == 1
+        elseif outSim(3,i) < (outSim(1,i) + outSim(2, i) + outSim(4, i)) && Target(3,i) == 1
             FN = FN + 1;
         end
+        i = i + 1;
     end
     sensi = TP / (TP + FN);
     speci = TN / (TN + FP);

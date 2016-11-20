8ad96ff9957fff53d0a0cc9e08e320bdd8bddb14
Joel Pinto <joel.pinto.correia@gmail.com>
2016-11-20 22:01:36 +0000

    added percentage calculation of  Preictal and Ictal detected
    calcPerform() now returns 4 values
    [sensi, speci, PreicPerc, IctalPerc] = calcPerform(outSim, TT);

--- calcPerform.m
+++ calcPerform.m
@@ -1,4 +1,4 @@
-function [sensi, speci] = calcPerform(outSim, Target)
+function [sensi, speci, PreicPerc, IctalPerc] = calcPerform(outSim, Target)
     i = 1;
     Trglength = size(Target);
     TP = 0; % true positives
@@ -6,20 +6,47 @@ function [sensi, speci] = calcPerform(outSim, Target)
     TN = 0; % true negatives
     FP = 0; % false positives
     
+    TotalTruePreic = 0; % total true Preictal points
+    TruePreic = 0;      % ture Preictal points found
+    TotalTrueIctal = 0; % total true Ictal points
+    TrueIctal = 0;      % true Ictal points found
+    
     while i < Trglength(1,2,1)
-        if outSim(3,i) > outSim(1,i) && outSim(3,i) > outSim(2, i) && outSim(3,i) > outSim(4, i) && Target(3,i) == 1
+        if outSim(3,i) > outSim(1,i) && outSim(3,i) > outSim(2,i) && outSim(3,i) > outSim(4,i) && Target(3,i) == 1
             TP = TP + 1;
-        elseif outSim(3,i) < outSim(1,i) && outSim(3,i) < outSim(2, i) &&outSim(3,i) < outSim(4, i) && Target(3,i) ~= 1
+        elseif outSim(2,i) > outSim(1,i) && outSim(2,i) > outSim(3,i) && outSim(2,i) > outSim(4,i) && Target(2,i) == 1
+            TP = TP + 1;
+        elseif (outSim(3,i) < outSim(1,i) || outSim(3,i) < outSim(2, i) || outSim(3,i) < outSim(4,i)) && Target(3,i) ~= 1
+            TN = TN + 1;
+        elseif (outSim(2,i) < outSim(1,i) || outSim(2,i) < outSim(3, i) || outSim(2,i) < outSim(4,i)) && Target(2,i) ~= 1
             TN = TN + 1;
         end
-        if outSim(3,i) > outSim(1,i) && outSim(3,i) > outSim(2, i) && outSim(3,i) > outSim(4, i) && Target(3,i) ~= 1
+        if outSim(3,i) > outSim(1,i) && outSim(3,i) > outSim(2,i) && outSim(3,i) > outSim(4,i) && Target(3,i) ~= 1
             FP = FP + 1;
-        elseif outSim(3,i) < outSim(1,i) && outSim(3,i) < outSim(2, i) && outSim(3,i) < outSim(4, i) && Target(3,i) ==1
+        elseif outSim(2,i) > outSim(1,i) && outSim(2,i) > outSim(3,i) && outSim(2,i) > outSim(4,i) && Target(2,i) ~= 1
+            FP = FP + 1;
+        elseif (outSim(3,i) < outSim(1,i) && outSim(3,i) < outSim(2,i) && outSim(3,i) < outSim(4,i)) && Target(3,i) ==1
             FN = FN + 1;
+        elseif (outSim(2,i) < outSim(1,i) && outSim(2,i) < outSim(3,i) && outSim(2,i) < outSim(4,i)) && Target(2,i) ==1
+            FN = FN + 1;
+        end
+        if Target(2, i) == 1
+            if outSim(2,i) > outSim(1,i) && outSim(2,i) > outSim(3, i) && outSim(2,i) > outSim(4, i)
+                TruePreic = TruePreic + 1;
+            end
+            TotalTruePreic = TotalTruePreic + 1;
+        end
+        if Target(3, i) == 1
+            if outSim(3,i) > outSim(1,i) && outSim(3,i) > outSim(2, i) && outSim(3,i) > outSim(4, i)
+                TrueIctal = TrueIctal + 1;
+            end
+            TotalTrueIctal = TotalTrueIctal + 1;
         end
         i = i + 1;
     end
     sensi = TP / (TP + FN);
     speci = TN / (TN + FP);
+    PreicPerc = (TruePreic / TotalTruePreic) * 100;
+    IctalPerc = (TrueIctal / TotalTrueIctal) * 100;
     return
 end

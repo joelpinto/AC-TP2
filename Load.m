4289ba41e7a0ba246a1bd9e079f2ae3bc1496d97
josemarcelino <jose.m.s.marcelino@gmail.com>
2016-11-21 23:18:07 +0000

    ui to load

---
+++ Load.m
@@ -0,0 +1,32 @@
+
+    load(uigetfile('*.mat','Select the MATLAB NN file'));
+
+    
+    %gpuDevice(1);
+
+    load(dataset);
+
+    
+    [TTTarget,BreakingPoints] = getTarget(Trg);
+    TTTarget = transpose(TTTarget);
+
+    BreakingPoints = BreakingPoints';
+
+    FinalTarget =[];
+    FinalIsolated =[];
+    FeatVectSel = FeatVectSel';
+
+
+    for iterator = 1:size(BreakingPoints)
+        before = BreakingPoints(iterator)-1000;
+        after = BreakingPoints(iterator)+ 1000;
+        FinalTarget = [FinalTarget,TTTarget(1:4, before:after)];
+        FinalIsolated = [FinalIsolated,FeatVectSel(1:29,before:after)];
+    end
+
+    outSim = sim(net,FinalIsolated);
+    [sensi, speci, PreicPerc, IctalPerc] = calcPerform(outSim, FinalTarget);
+
+
+
+

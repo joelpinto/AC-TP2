4d2d6621cf73dba588d8fd33ac5b840648fe6f71
josemarcelino <jose.m.s.marcelino@gmail.com>
2016-11-21 22:49:13 +0000

    ui with load from lsit

--- Classify.m
+++ Classify.m
@@ -1,7 +1,41 @@
-function [ output_args ] = Classify( input_args )
-%CLASSIFY Summary of this function goes here
-%   Detailed explanation goes here
-x = 'CLASSIFY'
 
-end
+
+%     dataset = '44202.mat';
+%     activation = 'purelin';
+%     neuralNetwork = 'feedfo';
+%     trainFunction ='trainscg'; 
+%     hiddenValue = 30;
+
+
+    if(strcmp(neuralNetwork,'feedfo_1'))
+        load('netfeedfo_trainlm_20.mat');
+    elseif(strcmp(neuralNetwork,'recnet_1'))
+        load('netfeedfo_trainscg_20.mat');
+    end
+    
+    
+    %gpuDevice(1);
+
+    load(dataset);
+
+    
+    [TTTarget,BreakingPoints] = getTarget(Trg);
+    TTTarget = transpose(TTTarget);
+
+    BreakingPoints = BreakingPoints';
+
+    FinalTarget =[];
+    FinalIsolated =[];
+    FeatVectSel = FeatVectSel';
+
+
+    for iterator = 1:size(BreakingPoints)
+        before = BreakingPoints(iterator)-1000;
+        after = BreakingPoints(iterator)+ 1000;
+        FinalTarget = [FinalTarget,TTTarget(1:4, before:after)];
+        FinalIsolated = [FinalIsolated,FeatVectSel(1:29,before:after)];
+    end
+
+    outSim = sim(net,FinalIsolated);
+    [sensi, speci, PreicPerc, IctalPerc] = calcPerform(outSim, FinalTarget);
 
